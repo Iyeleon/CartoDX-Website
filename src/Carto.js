00ae20cb3512626ba77050dc2b49ee0945481215
DamiAkinniyi <oluwadamilola.akinniyi@students.iaac.net>
2022-07-26 11:43:59 -0500

    initial push

---
+++ src/Carto.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './Carto.css';
+
+
+class CartoDx extends Component{
+    render(){
+        return(
+        <div id='wraps'>
+            <h1> Hello, Welcome to CartoDx Project. A project mapping cities vulnerability to malaria</h1>
+        </div> 
+        );
+    }
+}
+
+export default CartoDx
